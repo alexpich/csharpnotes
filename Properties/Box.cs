01ea57150aa81927e40795e116fba7c370d34796
alexpich <pich.alex01@gmail.com>
2019-06-11 21:48:45 -0700

    Added Properties

---
+++ Properties/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Box
+    {
+        private int length;
+        private int height;
+        //private int width;
+        private int volume;
+
+        // auto implemented property (type "prop" hit tab tab)
+        public int Width { get; set; }
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (value < 0)
+                    value = -value;
+                //throw new Exception("Size needs to be positive.");
+                height = value;
+            }
+        }
+
+        public int Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.Write("Length is {0}, Height is {1}, Width is {2}, and so the volume is {3}!", length, height, Width, volume = length * Width * height);
+        }
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------------------------
+            // Box class
+            //Box box = new Box();
+            //box.Length = 5;
+            //box.Width = 10;
+            //box.Height = 4;
+
+            //box.DisplayInfo();
+
+            //Console.ReadKey();
+
+            //------------------------------
+            // Members class
+            Members member1 = new Members();
+            member1.Introducing(true);
+
+        }
+    }
+}
