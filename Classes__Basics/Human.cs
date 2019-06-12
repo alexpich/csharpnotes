d50e78250ad8604613cb8c8bfbb0f665ef629ab6
alexpich <pich.alex01@gmail.com>
2019-06-11 19:25:56 -0700

    add Classes

---
+++ Classes__Basics/Human.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes__Basics
+{
+    // this class is a blueprint for a datatype
+    class Human
+    {
+        // member variable
+        public string firstName;
+        public string lastName;
+
+        // Constructor
+        public Human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        // member
+        public void IntroduceYourself()
+        {
+            Console.WriteLine("Hello, I'm {0} {1}", firstName, lastName);
+        }
+    }
+}
---
+++ Classes__Basics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes__Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human alex = new Human("alex", "p");
+
+            alex.IntroduceYourself();
+
+            Console.ReadKey();
+        }
+    }
+}
