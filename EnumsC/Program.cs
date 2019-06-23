2d704f3ca6638ad710ae0de4ca4cc23390bf677a
Alex Pich <pich.alex01@gmail.com>
2019-06-23 12:48:44 -0700

    Enums

---
+++ EnumsC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsC
+{
+    enum Day { Mo, Tu, We, Th, Fr, Sa, Su };
+    enum Month { Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day fr = Day.Fr;
+            Day sun = Day.Su;
+
+            Day a = Day.Fr;
+
+            Console.WriteLine(fr == a);
+
+            Console.WriteLine(Day.Mo);
+            Console.WriteLine((int)Day.Mo);
+
+            Console.WriteLine((int)Month.Feb);
+
+            Console.ReadKey();
+        }
+    }
+}
